1ac8362870244358ff2b2d98006c31f8600ff88a
Shruti-Asgekar <shrutiasgekar1@gmail.com>
2025-10-15 17:02:07 +0530

    Final Changes

---
+++ android/app/src/main/java/com/eduforum/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.eduforum.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.eduforum.app',
+  appName: 'EduForum',
+  webDir: 'build',
+};
+
+export default config;
